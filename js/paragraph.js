0222e6bdb4c989d340bca5936a6048ce692eb6e7
Zakaria M <zak.mell45@gmail.com>
2021-05-09 21:39:28 +0200

    added strike through paragraphs

--- js/paragraph.js
+++ js/paragraph.js
@@ -1,8 +1,13 @@
 function addParagraphs() {
 	let paragraph = document.createElement('p');
+	paragraph.className = 'paragraph-to-do';
 	paragraph.innerHTML = 'Hello World';
 	this.appendChild(paragraph);
 }
 function removeParagraphs() {
-	this.remove();
+	if (this.className == 'paragraph-done') {
+		this.className = 'paragraph-to-do';
+	} else if (this.className == 'paragraph-to-do') {
+		this.className = 'paragraph-done';
+	}
 }
