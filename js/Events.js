af71142057aab7b67e21632abe5ada7368197051
Zakaria M <zakaria.mellah.pro@gmail.con>
2021-05-17 09:27:48 +0200

    added buttons below each paragraphs to remove them

--- js/Events.js
+++ js/Events.js
@@ -4,32 +4,17 @@ const buttonTacheTerminee = document.getElementById('strikeThroughParagraphs');
 const buttonRemoveParagraphs = document.getElementById('removeParagraphs');
 const tableOfTache = document.getElementById('table_taches');
 const cellsOfTableTache = tableOfTache.getElementsByTagName('td');
-/*
-function addParagraphs() {
-	let paragraph = document.createElement('p');
-	paragraph.innerHTML = 'Hello World';
-	this.appendChild(paragraph);
-}
-function strikeThroughParagraphs() {
-	this.remove();
-}*/
 // Add Events
 function addParagraphEvents() {
 	for (const cell of cellsOfTableTache) {
 		cell.addEventListener('click', addParagraphs);
-		cell.removeEventListener('click', removeParagraphs);
-	}
-}
-function removeParagraphEvents() {
-	for (const cell of cellsOfTableTache) {
-		cell.removeEventListener('click', addParagraphs);
-		cell.addEventListener('click', removeParagraphs);
+		//cell.removeEventListener('click', removeParagraphs);
 	}
 }
 function strikeThroughEvents() {
 	for (const cell of cellsOfTableTache) {
 		cell.removeEventListener('click', addParagraphs);
-		cell.removeEventListener('click', removeParagraphs);
+		//cell.removeEventListener('click', removeParagraphs);
 		let paragraphs = cell.getElementsByTagName('p');
 		for (let i = 0; i < paragraphs.length; ++i) {
 			paragraphs[i].addEventListener('click', strikeThroughParagraphs);
@@ -38,4 +23,3 @@ function strikeThroughEvents() {
 }
 buttonAddParagraphs.addEventListener('click', addParagraphEvents);
 buttonTacheTerminee.addEventListener('click', strikeThroughEvents);
-buttonRemoveParagraphs.addEventListener('click', removeParagraphEvents);
--- js/paragraph.js
+++ js/paragraph.js
@@ -1,8 +1,22 @@
 function addParagraphs() {
+	let div = document.createElement('div');
 	let paragraph = document.createElement('p');
+	let btn = document.createElement('button');
+	let btnEdit = document.createElement('button');
+	btnEdit.type = 'button';
+	btnEdit.innerText - 'Modifier';
+	btn.type = 'button';
+	btn.innerText = 'Supprimer';
 	paragraph.className = 'paragraph-to-do';
-	paragraph.innerHTML = 'Hello World';
-	this.appendChild(paragraph);
+	paragraph.innerText = prompt('Contenu du paragraphe:');
+	if (paragraph.innerText != '') {
+		div.appendChild(paragraph);
+		div.appendChild(btn);
+		this.appendChild(div);
+		btn.addEventListener('click', function () {
+			this.parentElement.remove();
+		});
+	}
 }
 function strikeThroughParagraphs() {
 	if (this.className == 'paragraph-done') {
@@ -10,8 +24,4 @@ function strikeThroughParagraphs() {
 	} else if (this.className == 'paragraph-to-do') {
 		this.className = 'paragraph-done';
 	}
-}
-
-function removeParagraphs(e) {
-	this.remove();
 }
